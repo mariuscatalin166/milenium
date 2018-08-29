ce3f2e1b2090d0c57570d7e5193e7181972dd465
razvanlikespizza <razvanro666@gmail.com>
2018-08-29 13:01:32 +0300

    new stuff added

---
+++ js/hala-interactivity.js
@@ -0,0 +1,33 @@
+var labelGrinda = document.getElementById("label-grinda"),
+		labelEuro = document.getElementById("label-euro"),
+		imgReprezentare1 = document.getElementById("img-reprezentare1"),
+		imgReprezentare2 = document.getElementById("img-reprezentare2"),
+		inputOpeningImg = document.getElementById("deschidere"),
+		spanOpeningImg = document.getElementsByClassName("opening"),
+		inputHeightImg = document.getElementById("inaltime"),
+		spanHeightImg = document.getElementById("height"),
+		inputWidthImg = document.getElementById("lungime"),
+		spanWidthImg = document.getElementById("width");
+spanOpeningImg = Array.from(spanOpeningImg);
+
+labelGrinda.addEventListener("click", function() {
+	imgReprezentare1.src = "assets/img/hala2d1.png";
+});
+labelEuro.addEventListener("click", function() {
+	imgReprezentare1.src = "assets/img/hala2d2.png";
+});
+inputOpeningImg.addEventListener("input", function() {
+	spanOpeningImg.forEach(function(el) {
+		el.innerText = inputOpeningImg.value + " m";
+	});
+	this.value != "" ? inputHeightImg.disabled = false : inputHeightImg.disabled = true;
+});
+inputHeightImg.addEventListener("input", function() {
+	spanHeightImg.innerText = this.value + " m";
+	this.value != "" ? inputWidthImg.disabled = false : inputWidthImg.disabled = true;
+});
+inputWidthImg.addEventListener("input", function() {
+	spanWidthImg.innerText = this.value + " m";
+	imgReprezentare2.src = "assets/img/hala3d.png";
+	this.value != "" ? document.getElementsByClassName("interactivity-image")[1].classList.remove("hidden") : document.getElementsByClassName("interactivity-image")[1].classList.add("hidden");
+});
